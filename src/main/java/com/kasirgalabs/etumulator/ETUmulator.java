39a3dc35c53825e3eb6fc1057ae90e1db7eb91f8
burak <burakbaran@etu.edu.tr>
2018-03-22 16:55:39 +0300

    Popup creation is done

--- src/main/java/com/kasirgalabs/etumulator/ETUmulator.java
+++ src/main/java/com/kasirgalabs/etumulator/ETUmulator.java
@@ -160,9 +160,57 @@ public class ETUmulator extends Application {
         });
         OutputStream out = new OutputStream(){
             private TextArea console = new TextArea();
+
+            public void createPopup(){
+                //console.setMouseTransparent(true);
+                //console.setFocusTraversable(false);
+                console.setEditable(false);
+                Stage stage = new Stage();
+                stage.setTitle("Error(s)!");
+                VBox box = new VBox();
+                box.setPadding(new Insets(5));
+                box.setAlignment(Pos.CENTER);
+                Button btnClose = new Button();
+                btnClose.setText("Close");
+                btnClose.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        stage.close();
+                        console.clear();
+                        
+                    }
+                });
+                stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                    @Override 
+                    public void handle(WindowEvent t) {
+                        stage.close();
+                        console.clear();
+                        
+                    }
+                });
+                Button btnClear = new Button();
+                btnClear.setText("Clear");
+                btnClear.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        console.clear();
+                    }
+                });
+                box.getChildren().add(console);
+                box.getChildren().add(btnClose);
+                box.getChildren().add(btnClear);
+                Scene scene1 = new Scene(box, 450, 350);
+                stage.setScene(scene1);
+                stage.show();
+                stage.show();
+                       
+            }            
             @Override
             public void write(int b) throws IOException {
                 Platform.runLater(() -> console.appendText(String.valueOf((char)b)));
+                if((char)b == '\n'){
+                    createPopup();
+                }
             }
         };
         System.setErr(new PrintStream (out));
